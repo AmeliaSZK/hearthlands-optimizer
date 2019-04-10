c1b64ceb08b7dd87d16988793d8581d9682cd5fc
Amélia <slayzekyriarchy@protonmail.com>
2019-04-10 00:43:02 -0400

    Resource is a potential candidate for release.
    Package-info still has questions in its Javadoc.

---
+++ package-info.java
@@ -0,0 +1,10 @@
+/**
+ * A Java program to support an Excel spreadsheet meant to optimize gameplay of
+ * Hearthlands.
+ * 
+ * https://github.com/SlayZeKyriarchy/hearthlands-optimizer
+ * 
+ * Eclipse suggested this file and I do not know why I would want one.
+ * Explanations welcomed. Is it only to centralize the javadoc??
+ */
+package hearthlandsoptimizer;
